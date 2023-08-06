077e73540827c58a4bbeae2183e78d1d1c12bf95
khosta77 <puwerfulpants@gmail.com>
2023-08-06 22:47:04 +0300

    Написал пример использования библиотеки

--- project/main.cpp
+++ project/main.cpp
@@ -9,41 +9,90 @@ int main([[ maybe_unused ]] int argc, [[ maybe_unused ]] const char **argv) {
     return 0;
 }
 
-double Big_Work() {
-    const size_t SIZE = 100000;
+double Big_Work1() {
+    const size_t SIZE = 100;
     std::vector<std::vector<int>> vec;
     for (size_t i = 0; i < SIZE; i++) {
         std::vector<int> vec_in;
         for (size_t j = 0; j < SIZE; j++) {
-            vec_int.push_back(j);
+            vec_in.push_back(j);
         }
         vec.push_back(vec_in);
         for (size_t j = 0; j < SIZE; j++) {
             vec_in.pop_back();
         }
-        vec.clear();
+        vec_in.clear();
     }
 
-    for (size_t i = 0; i < SIZE; i++) {
-        for (size_t j = 0; j < SIZE; j++) {
-            vec[i].pop_back();
-            vec[i].clear();
-        }
-    }
-
-    for (size_t i = 0; i < SIZE; i++) {
-        vec.pop_back();
-    }
     vec.clear();
     return 0;
 }
 
+double Big_Work2() {
+    const size_t SIZE = 1000;
+    std::vector<std::vector<int>> vec;
+    for (size_t i = 0; i < SIZE; i++) {
+        std::vector<int> vec_in;
+        for (size_t j = 0; j < SIZE; j++) {
+            vec_in.push_back(j);
+        }
+        vec.push_back(vec_in);
+        for (size_t j = 0; j < SIZE; j++) {
+            vec_in.pop_back();
+        }
+        vec_in.clear();
+    }
+
+    vec.clear();
+    return 0;
+}
+
+double Big_Work3() {
+    const size_t SIZE = 10000;
+    std::vector<std::vector<int>> vec;
+    for (size_t i = 0; i < SIZE; i++) {
+        std::vector<int> vec_in;
+        for (size_t j = 0; j < SIZE; j++) {
+            vec_in.push_back(j);
+        }
+        vec.push_back(vec_in);
+        for (size_t j = 0; j < SIZE; j++) {
+            vec_in.pop_back();
+        }
+        vec_in.clear();
+    }
+
+    vec.clear();
+    return 0;
+}
+
+
+
 void test() {
-    TimeTest tt(std::string("Apple Silicon M1 Pro 14+8"));
-    tt.add(std::string("Проверка группы"),
-           std::string("Имя теста"),
-           Big_Work);
-    DataFrameFunction::print_pair_resultdf(tt.dotests());
+#if 0
+    STSL::ResultsOut ro;
+    ro.readResultFromCsv("Apple Silicon M1 Pro 14+8.csv");
+    ro.outTexResults();
+#else
+    STSL::TimeTest tt(std::string("Apple Silicon M1 Pro 14+8"));
+    const std::string group_first = "Проверочная группа";
+    tt.add(group_first,
+           std::string("Тест 1, на 100 элементов"),
+           Big_Work1);
+    tt.add(group_first,
+           std::string("Тест 2, на 1000 элементов"),
+           Big_Work2);
+    tt.add(group_first,
+           std::string("Тест 3, на 10000 элементов"),
+           Big_Work3);
+    auto rtt = tt.dotests();
+    STSL::DataFrameFunction::print_pair_resultdf(rtt);
+    STSL::ResultsOut ro(rtt);
+    
+    ro.outTexResults();
+
+    ro.writeResultsToCsv();
+#endif
 }
 
 
